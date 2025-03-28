678e144ca3946ea5199cd99e0b939273df278ddd
SiyingXu <73036875+SiyingXu@users.noreply.github.com>
2025-03-28 23:21:17 +0100

    Create train_vicreg.py

---
+++ feature_learning/train/train_vicreg.py
@@ -0,0 +1,39 @@
+
+import os
+import time
+import datetime
+import tensorflow as tf
+
+from utils.callbacks import get_callbacks
+from data_loader.feature_vicreg_data import CINE2DDataset
+from feature_learning.losses.vicreg_loss import get_loss
+from feature_learning.models.VICReg_feature_model import UNET2dt
+
+
+def train_feature_learning(min_R, max_R, num_iter, split=None, fold='vicreg_feature'):
+    ds_train = CINE2DDataset(min_R, max_R, mode='train', split=split, shuffle=True)
+    ds_val = CINE2DDataset(min_R, max_R, mode='val', split=split, shuffle=False)
+
+    model = UNET2dt(num_iter=num_iter, mode='train')
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0004)
+    model.compile(optimizer=optimizer, loss=get_loss('loss_VICReg_unroll'), run_eagerly=True)
+
+    # initialize model to print model summary
+    inputs, targets = ds_train.__getitem__(0)
+    start = time.time()
+    outputs = model.predict(inputs)
+    end = time.time()
+    print(end - start)
+    print(model.summary())
+
+    # Logging dir
+    exp_dir = f'fold__{fold}__/R__{min_R}__{max_R}'
+    log_dir = os.path.join(exp_dir, model.name + '-' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    # Training
+    model.fit(ds_train, epochs=200, validation_data=ds_val, max_queue_size=4,
+              callbacks=get_callbacks(model, log_dir), workers=2)
+
+
+if __name__ == '__main__':
+    train_feature_learning(min_R=2, max_R=16, num_iter=2)
