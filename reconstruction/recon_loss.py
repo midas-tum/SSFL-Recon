34c5d280fde8405d962ab83ff36db98c9e3a3de8
SiyingXu <73036875+SiyingXu@users.noreply.github.com>
2025-04-05 21:33:07 +0200

    Update recon_loss.py

--- reconstruction/recon_loss.py
+++ reconstruction/recon_loss.py
@@ -7,7 +7,7 @@ def get_loss(loss):
 
 
 @tf.function
-def img_ksp_loss(y_true, y_pred):
+def img_ksp_loss(y_true, y_pred):  # image and k-space losses are calculated in recon_model 
     img_loss = y_pred[0]
     ksp_loss = y_pred[1]
     total_loss = img_loss + ksp_loss
